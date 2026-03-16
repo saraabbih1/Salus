1cfb5d7ce64d288afae9e39a736f6a1437958437
saraabbih1 <abbihsara30@gmail.com>
2026-03-16 13:46:44 +0000

    feat:doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -1,10 +1,28 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+      use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'specialty',
+        'city',
+        'yearsofexperience',
+        'consultation_price',
+        'available_days',
+    ];
+
+    protected $casts = [
+        'available_days' => 'array', 
+    ];
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
